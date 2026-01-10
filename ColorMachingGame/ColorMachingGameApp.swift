4c9949a36cdf656236078a98fed9397e36fc79f3
Yasodha Heshan <yasodhaheshan2002@gmail.com>
2026-01-10 09:48:14 +0530

    Initial Commit

---
+++ ColorMachingGame/ColorMachingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMachingGameApp.swift
+//  ColorMachingGame
+//
+//  Created by COBSCCOMP242P-042 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct ColorMachingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
